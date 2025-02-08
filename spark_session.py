6c10f6f11f1169930f499808a7c47e85f90d2801
RH <rh@users.noreply.github.com>
2025-02-08 23:47:32 +0000

    spark session update

--- spark_session.py
+++ spark_session.py
@@ -1,15 +1,20 @@
 from pyspark.sql import SparkSession
-
+import os
+ 
 def get_spark():
     """
-    Create and return a Spark session.
-    
-    :param app_name: Name of the Spark application.
-    :param master: Spark master URL (default: local mode).
-    :return: SparkSession object.
+    Create and return a Spark session optimized for running on a GCP VM.
     """
+    # Automatically use the correct service account if running on a GCP VM
+    if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
+        print("Using VM's default service account for authentication.")
+ 
     spark = SparkSession.builder \
-        .appName('GCP') \
+        .appName('pyspark-run-with-gcp-bucket') \
+        .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.5") \
+        .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+        .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
+        .config("spark.sql.repl.eagerEval.enabled", True) \
         .getOrCreate()
     
     return spark
