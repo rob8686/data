c5b0957bd46b95e6bdc7b5a5f51a4e8e56dc6bd3
RH <rh@users.noreply.github.com>
2025-02-09 12:29:17 +0000

    Test

--- spark_session.py
+++ spark_session.py
@@ -11,10 +11,11 @@ def get_spark():
  
     spark = SparkSession.builder \
         .appName('pyspark-run-with-gcp-bucket') \
-        .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.5") \
-        .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
-        .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
+        .config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar") \
         .config("spark.sql.repl.eagerEval.enabled", True) \
         .getOrCreate()
     
+         #.config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.5") \
+        #.config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+        #.config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
     return spark
